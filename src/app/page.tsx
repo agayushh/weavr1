1977787ecfb70fc20aa46d6eaa70710264ee142f
agayushh <agayush088@gmail.com>
2025-10-28 17:30:45 +0530

    background job1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,23 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+
 import { requireAuth } from "@/lib/auth-utils";
 import { caller } from "@/trpc/server";
 import { Logout } from "./logout";
+import { useMutation, useQuery } from "@tanstack/react-query";
+import { useTRPC } from "@/trpc/client";
 
-export default async function () {
-  await requireAuth();
-  const data = await caller.getUsers();
+export default function () {
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.getWorkflows.queryOptions());
+  const create = useMutation(trpc.createWorkflow.mutationOptions({}));
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
       protected server component
       {JSON.stringify(data, null, 2)}
+      <Button disabled={create.isPending} onClick={() => create.mutate()}>Create Workflow</Button>
       <Logout />
-
     </div>
   );
 }
