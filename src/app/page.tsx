b4fe5514d31cbc3cad2393d91c9c065e28a2ad9c
agayushh <agayush088@gmail.com>
2025-10-28 22:48:23 +0530

    added gemini, openAI, claude provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,15 +13,27 @@ export default function () {
   const trpc = useTRPC();
   const queryClient = useQueryClient();
   const { data } = useQuery(trpc.getWorkflows.queryOptions());
-  const create = useMutation(trpc.createWorkflow.mutationOptions({
-    onSuccess: () =>{
-      toast.success("Job queued");
-    }
-  }));
+  const create = useMutation(
+    trpc.createWorkflow.mutationOptions({
+      onSuccess: () => {
+        toast.success("Job queued");
+      },
+    })
+  );
+  const testAi = useMutation(
+    trpc.testAi.mutationOptions({
+      onSuccess: () => {
+        toast.success("AI Job queued");
+      },
+    })
+  );
   return (
     <div className="min-h-screen min-w-screen flex items-center justify-center flex-col gap-y-6">
       protected server component
       <div>{JSON.stringify(data, null, 2)}</div>
+      <Button disabled={testAi.isPending} onClick={() => testAi.mutate()}>
+        Test AI
+      </Button>
       <Button disabled={create.isPending} onClick={() => create.mutate()}>
         Create Workflow
       </Button>
