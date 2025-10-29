a2ad5eeb120db3287fb19c2a8eb444b114e356ae
agayushh <agayush088@gmail.com>
2025-10-29 06:38:15 +0530

    payments integrated

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,7 @@
 import { betterAuth } from "better-auth";
+import { checkout, polar, portal } from "@polar-sh/better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
+import { polarClient } from "./polar";
 import prisma from "./db";
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -8,6 +10,24 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
     autoSignIn: true,
-  }
+  },
+  plugins: [
+    polar({
+      client: polarClient,
+      createCustomerOnSignUp: true,
+      use: [
+        checkout({
+          products: [
+            {
+              productId: "28fb54d7-956c-4aa8-8d76-e0978ef1066f",
+              slug: "pro",
+            },
+          ],
+          successUrl: process.env.POLAR_SUCCESS_URL,
+          authenticatedUsersOnly: true,
+        }),
+        portal(),
+      ],
+    }),
+  ],
 });
-
